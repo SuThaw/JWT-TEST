de983c4eabd15129ec0610e55692436182b8e6de
Su Thaw <suthaw524@gmail.com>
2015-06-22 15:42:24 +0800

    Connect with mongodb and finish

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var jwt = require('jwt-simple');
 var _ = require('lodash');
 var bcrypt = require('bcrypt');
-
+var User = require('./user');
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -13,27 +13,44 @@ var users = [{username:'suthaw',password: bcrypt.hashSync('admin123',10)}];
 
 var secretKey = 'supersecretKey';
 
-function findUserByUsername(username){
-	return _.find(users,{username:username});
-}
+app.post('/user',function(req,res){
+	var user = new User({username:req.body.username});
+	bcrypt.hash(req.body.password,10,function(err,hash){
+		user.password = hash;
+		user.save(function(err,user){
+			console.log(user);
+			if(err) throw(err);
+			res.send(201);
+		});
+	})
+});
 
-function validateUser(user,password,cb){
-	return bcrypt.compare(password,user.password,cb)
-}
-app.post('/session',function(req,res){
-	var user = findUserByUsername(req.body.username);
-	validateUser(user,req.body.password,function(err,valid){
-		if(err || !valid) { return res.send(401);}
-		var token = jwt.encode({usernmae:user.username},secretKey);
-		res.json(token);
 
-	});
+app.post('/session',function(req,res,next){
+	User
+		.findOne({username:req.body.username})
+		.select('password')
+		.exec(function(err,user){
+			if(err) next(err);
+			if(!user) return res.send(401);
+			bcrypt.compare(req.body.password,user.password,function(err,valid){
+				if(err || !valid) return res.send(401);
+				console.log(user);
+				var token = jwt.encode({username:req.body.username},secretKey);
+				res.json(token);
+			});
+		});
+
 });
 
 app.get('/user',function(req,res){
 	var token = req.headers['x-auth'];
 	var user = jwt.decode(token,secretKey);
-	res.json(user);
+	console.log(user);
+	User.findOne({username:user.username},function(err,user){
+		if(err) throw(err);
+		res.json(user);
+	});
 });
 
 app.listen(3000);
